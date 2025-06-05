e859e0793ede62235a2db4687885804ad59021c9
Carlaisabelle <lyc.manavella@gmail.com>
2025-06-05 13:50:27 +0200

    Added js folder
    Updated assets folder: new folder with JS files added.

---
+++ assets/js/checkRequired.js
@@ -0,0 +1,20 @@
+//SCRIPT n.1: CONTROLLA SE I CAMPI SONO COMPILATI CORRETTAMENTE / SCRIPT n.1: CHECKING IF ALL FILEDS ARE FILLED CORRECTLY
+(() => {
+    
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
